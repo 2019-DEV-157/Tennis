d471857bb2ede27db96aa3dda935b81026ffd966
2019_DEV_157 <jdurieu@Jonathans-MacBook-Pro-2.local>
2019-05-08 15:14:34 +0200

    Game & GameTest creation

---
+++ Tennis/Game.swift
@@ -0,0 +1,19 @@
+//
+//  Game.swift
+//  Tennis
+//
+//  Created by 2019_DEV_157 on 08/05/2019.
+//  Copyright © 2019 2019_DEV_157. All rights reserved.
+//
+
+import Foundation
+
+public class Game {
+    private (set) var playerOne: Player
+    private (set) var playerTwo: Player
+    
+    init(firstPlayer: Player, secondPlayer: Player){
+        self.playerOne = firstPlayer
+        self.playerTwo = secondPlayer
+    }
+}
---
+++ TennisTests/GameTests.swift
@@ -0,0 +1,37 @@
+//
+//  GameTests.swift
+//  TennisTests
+//
+//  Created by 2019_DEV_157 on 08/05/2019.
+//  Copyright © 2019 2019_DEV_157. All rights reserved.
+//
+
+import XCTest
+
+class GameTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testGameInit() {
+        let playerOne = Player(playerName: "Player 1")
+        let playerTwo = Player(playerName: "Player 2")
+        
+        let game = Game(firstPlayer: playerOne, secondPlayer: playerTwo)
+        
+        XCTAssert(game.playerOne === playerOne)
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
