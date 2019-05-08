db9aa65cb41230600f0e7da768073bad50f8f102
2019_DEV_157 <jdurieu@Jonathans-MacBook-Pro-2.local>
2019-05-08 15:01:07 +0200

    Player init & playerTest

---
+++ Tennis/Player.swift
@@ -0,0 +1,20 @@
+//
+//  Player.swift
+//  Tennis
+//
+//  Created by 2019 2019_DEV_157 on 08/05/2019.
+//  Copyright © 2019 2019_DEV_157. All rights reserved.
+//
+
+import Foundation
+
+public class Player {
+    
+    private (set) var name:String
+    var points:Int
+    
+    public init(playerName:String) {
+        self.name = playerName
+        self.points = 0
+    }
+}
---
+++ TennisTests/PlayerTests.swift
@@ -0,0 +1,33 @@
+//
+//  PlayerTests.swift
+//  TennisTests
+//
+//  Created by 2019_DEV_157 on 08/05/2019.
+//  Copyright © 2019 2019_DEV_157. All rights reserved.
+//
+
+import XCTest
+
+class PlayerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testPlayerInit() {
+        var newPlayer = Player(playerName: "John")
+        XCTAssert(newPlayer.name == "John")
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
