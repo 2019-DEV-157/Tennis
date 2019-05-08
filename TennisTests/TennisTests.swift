c8d8846844859e106a11284bd851ecf529ae61dc
2019_DEV_157 <jdurieu@Jonathans-MacBook-Pro-2.local>
2019-05-08 14:55:27 +0200

    first test

--- TennisTests/TennisTests.swift
+++ TennisTests/TennisTests.swift
@@ -19,8 +19,7 @@ class TennisTests: XCTestCase {
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(true)
     }
 
     func testPerformanceExample() {
