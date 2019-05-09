6c11c242d8e8731914da6244e0773b94aab65c06
2019_DEV_157 <jdurieu@Jonathans-MacBook-Pro-2.local>
2019-05-09 15:19:15 +0200

    Game

--- Tennis/main.swift
+++ Tennis/main.swift
@@ -8,5 +8,43 @@
 
 import Foundation
 
-print("Hello, World!")
+var playerOne: Player
+var playerTwo: Player
+var game: Game
+
+playerOne = Player(playerName: "John")
+playerTwo = Player(playerName: "David")
+
+game = Game(firstPlayer: playerOne, secondPlayer: playerTwo)
+
+//Fifteen - Love
+game.playerWinPoint(player: playerOne)
+print(game.getScore())
+//Fifteen - Fifteen
+game.playerWinPoint(player: playerTwo)
+print(game.getScore())
+//Fifteen - Thirty
+game.playerWinPoint(player: playerTwo)
+print(game.getScore())
+//Thirty - Thirty
+game.playerWinPoint(player: playerOne)
+print(game.getScore())
+//Thirty - Forty
+game.playerWinPoint(player: playerTwo)
+print(game.getScore())
+//Deuce
+game.playerWinPoint(player: playerOne)
+print(game.getScore())
+//Advantage John
+game.playerWinPoint(player: playerOne)
+print(game.getScore())
+//Deuce
+game.playerWinPoint(player: playerTwo)
+print(game.getScore())
+//Advantage John
+game.playerWinPoint(player: playerOne)
+print(game.getScore())
+//John wins
+game.playerWinPoint(player: playerOne)
+print(game.getScore())
 
